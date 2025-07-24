b766e1521b901f625178f16224cbb1b0b699f04e
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-07-24 14:48:26 +0700

    Setup PWA

--- Frontend/src/layouts/root-layout.tsx
+++ Frontend/src/layouts/root-layout.tsx
@@ -1,15 +1,14 @@
 import SiteHeader from "@/components/Navbar";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Outlet } from "react-router";
 
 function RootLayout() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <>
             <SiteHeader />
             <div className="relative flex min-h-svh flex-col bg-background">
                 <Outlet />
             </div>
-        </ThemeProvider>
+        </>
     );
 }
 
